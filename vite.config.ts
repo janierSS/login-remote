495492d1c3984299fbd1784d81a5fdd02577e7a2
Janier <janierss17@gmail.com>
2024-12-02 11:55:02 -0500

    Create app

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), federation({
+    name: 'loginRemote',
+    filename: 'remoteEntry.js',
+    remotes: {},
+    exposes: {
+      // './NotesHome': './src/components/NotesHome'
+    },
+    shared: ['react', 'react-dom', 'react-redux', '@reduxjs/toolkit']
+  })],
+  server: {
+    open: true,
+    port: 5001
+  },
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
+})
